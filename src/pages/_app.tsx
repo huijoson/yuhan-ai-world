4cabb25677c407c08634b4498c0e825aa1a843ae
Lawrence <hlchimeilawrence@gmail.com>
2023-04-12 23:39:25 +0800

    add appProps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import { ApolloProvider } from '@apollo/client'
-
+import { AppProps } from 'next/app'
 import { useApollo } from '@/lib/apolloClient'
 
 import '../styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     const apolloClient = useApollo(pageProps)
 
     return (
